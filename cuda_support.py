00f44045fc217dac275862a22ae25720175cd47d
xiaoran <xiaoran2495897824@163.com>
2024-02-25 13:31:50 +0800

    update: cuda_support.py

---
+++ cuda_support.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import re
+from typing import Sequence, Tuple
+
+from torch.utils.cpp_extension import include_paths as torch_include_paths
+
+
+def get_supported_cuda_archs() -> Sequence[Tuple[int, int]]:
+    cuda_config_h = Path(torch_include_paths()[0]) / "ATen" / "cuda" / "CUDAConfig.h"
+    if not cuda_config_h.exists():
+        return []
+
+    nvcc_flags_re = re.compile(r'^#define NVCC_FLAGS_EXTRA "([^"]+)"', flags=re.MULTILINE)
+    match = nvcc_flags_re.search(cuda_config_h.read_text())
+    if match is None:
+        return []
+    nvcc_flags = match.group(1)
+
+    compute_arch_re = re.compile(r"-gencode;arch=compute_(\d+)")
+    cuda_archs = []
+    for arch in compute_arch_re.findall(nvcc_flags):
+        cuda_archs.append(divmod(int(arch), 10))
+
+    return sorted(cuda_archs)
+
+
+print(get_supported_cuda_archs())
