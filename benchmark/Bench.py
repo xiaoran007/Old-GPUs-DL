00251f9cc83b709edf0159749504486c5996bcc7
xiaoran <xiaoran2495897824@163.com>
2024-05-20 00:20:07 +0800

    update: resnet bench

--- benchmark/Bench.py
+++ benchmark/Bench.py
@@ -1,13 +1,14 @@
 import torch
 import sys
 from benchmark.bench.cnn_bench import CNNBench
+from benchmark.bench.resnet50_bench import ResNet50Bench
 
 
 class Bench(object):
-    def __init__(self, method="cnn", auto=True, size=1024, epochs=10):
+    def __init__(self, method="cnn", auto=True, size=1024, epochs=10, batch_size=4):
         self.gpu_device = self._get_gpu_device()
         self.cpu_device = self._get_cpu_device()
-        self.backend = self._load_backend(method=method, auto=auto, size=size, epochs=epochs)
+        self.backend = self._load_backend(method=method, auto=auto, size=size, epochs=epochs, batch_size=batch_size)
 
     def start(self):
         self.backend.start()
@@ -32,17 +33,25 @@ class Bench(object):
         else:
             return 0
 
-    def _load_backend(self, method, auto, size, epochs):
+    def _load_backend(self, method, auto, size, epochs, batch_size):
+        if auto:
+            cuda_memory_size = self._get_cuda_memory_size()
+            data_size = int(int((cuda_memory_size / 12296) / 100) * 100 * 0.7)
+            epochs = 10
+        else:
+            self._get_cuda_memory_size()
+            data_size = int(int((size * 1024 * 1024 / 12296) / 100) * 100)
+        print(f"Set model to {method}, set data size to {data_size} images, total memory size: {data_size * 12296 / 1024 / 1024:.2f} MB")
         if method == "cnn":
-            if auto:
-                cuda_memory_size = self._get_cuda_memory_size()
-                data_size = int(int((cuda_memory_size / 12296)/100) * 100 * 0.7)
-                print(f"Set data size to {data_size} images, total memory size: {data_size * 12296 / 1024 / 1024:.2f} MB")
-                return CNNBench(gpu_device=self.gpu_device, cpu_device=self.cpu_device, data_size=data_size,
-                                batch_size=2048, epochs=10)
-            else:
-                data_size = int(int((size * 1024 * 1024 / 12296) / 100) * 100)
-                print(
-                    f"Set data size to {data_size} images, total memory size: {data_size * 12296 / 1024 / 1024:.2f} MB")
-                return CNNBench(gpu_device=self.gpu_device, cpu_device=self.cpu_device, data_size=data_size, batch_size=2048, epochs=epochs)
+            if batch_size == 0:
+                batch_size = 2048
+            return CNNBench(gpu_device=self.gpu_device, cpu_device=self.cpu_device, data_size=data_size,
+                            batch_size=batch_size, epochs=epochs)
+        elif method == "resnet50":
+            if batch_size == 0:
+                batch_size = 4
+            return ResNet50Bench(gpu_device=self.gpu_device, cpu_device=self.cpu_device, data_size=data_size,
+                                 batch_size=batch_size, epochs=epochs)
+
+
 
